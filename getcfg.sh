b8dbdc020425280653493dbb1f537140a87b5f19
Per Erskjans <per.erskjans@gmail.com>
2020-04-04 18:00:12 +0200

    getcfg.sh.

--- getcfg.sh
+++ getcfg.sh
@@ -5,9 +5,9 @@
 #
 
 
-git clone --bare https://github.com/perskjans/.cfg.git $HOME/.cfg
+git clone --bare https://github.com/perskjans/.cfg.git
 
-args="--git-dir=$HOME/.cfg/ --work-tree=$HOME"
+args="--git-dir=$HOME/.cfg.git/ --work-tree=$HOME"
 
 git $args checkout -f
 git $args config --local status.showUntrackedFiles no
